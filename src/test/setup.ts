03c97a939ba72e3bee0ad205a066a349643d3e5c
Kinrezc <matt.czernik@gmail.com>
2024-11-15 17:17:01 -0500

    update addrs

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -23,13 +23,13 @@ export async function setupTestEnvironment() {
   // Deploy your contracts here and get their addresses
   // You'll need to deploy: poolManager, airlock, tokenFactory, etc.
   const addresses = {
-    airlock: '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0' as Address,
-    tokenFactory: '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9' as Address,
-    dopplerFactory: '0x0165878A594ca255338adfa4d48449f69242Eb8F' as Address,
-    governanceFactory: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9' as Address,
-    migrator: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707' as Address,
+    airlock: '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707' as Address,
+    tokenFactory: '0x0165878A594ca255338adfa4d48449f69242Eb8F' as Address,
+    dopplerFactory: '0x8A791620dd6260079BF849Dc5567aDC3F2FdC318' as Address,
+    governanceFactory: '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853' as Address,
+    migrator: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6' as Address,
     poolManager: '0x5FbDB2315678afecb367f032d93F642f64180aa3' as Address,
-    stateView: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512' as Address,
+    stateView: '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9' as Address,
   };
 
   const sdk = new DopplerSDK(
