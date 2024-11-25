830f1ff2ce95b2afd86063a516bc86ec9a4818db
kinrezc <matt.czernik@gmail.com>
2024-11-25 15:50:09 -0500

    fix build

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,23 @@ export * from './entities';
 export * from './addresses';
 export * from './types';
 export * from './actions';
+
+export type { 
+  DopplerAddresses,
+  Clients,
+  TokenConfig,
+  DeploymentConfigParams 
+} from './types';
+
+export type {
+  HookConfig,
+  HookState,
+  Position,
+  PoolState
+} from './entities/Doppler';
+
+export type {
+  DopplerPreDeploymentConfig,
+  DopplerDeploymentConfig,
+  PriceRange
+} from './entities/Deployer';
