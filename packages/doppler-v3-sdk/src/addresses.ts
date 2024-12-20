c6ee0c06fb89d46105da593fd15b9072752e5b33
kinrezc <matt.czernik@gmail.com>
2024-12-20 17:13:56 -0500

    clean up create flow + conditional tick setting

--- packages/doppler-v3-sdk/src/addresses.ts
+++ packages/doppler-v3-sdk/src/addresses.ts
@@ -4,11 +4,10 @@ import { DopplerAddresses } from './types';
 export const DOPPLER_ADDRESSES: { [chainId: number]: DopplerAddresses } = {
   // unichain sepolia
   1301: {
-    dopplerFactory: '0x4c5859f0f772848b2d91f1d83e2fe57935348029' as Address,
     airlock: '0x379543D196F9ea360084bb61f424780d7E7eBEC3' as Address,
     tokenFactory: '0xC87Dd16242545C0BBa03bA8d36AFaC6B5d566b4e' as Address,
     v3Initializer: '0x06d0fd396c59401c68aD3E363De845fE61DFA6A9' as Address,
     governanceFactory: '0xa4E1915bBe3C09EeC16916b880773E5af58b2578' as Address,
-    migrator: '0xFC9b263ec6509adBc8aDF185ab40643737372927' as Address,
+    liquidityMigrator: '0xFC9b263ec6509adBc8aDF185ab40643737372927' as Address,
   },
 };
--- packages/doppler-v3-sdk/src/test/deploy/setup.ts
+++ packages/doppler-v3-sdk/src/test/deploy/setup.ts
@@ -75,15 +75,14 @@ export async function setupTestEnvironment(): Promise<TestEnvironment> {
 
   // Deploy your contracts here and get their addresses
   // You'll need to deploy: poolManager, airlock, tokenFactory, etc.
+  // NOTE: THIS DOES NOT WORK RIGHT NOW. ADDRESSES ARE NOT CORRECT.
   const addresses = {
     airlock: contractAddresses[0] as Address,
     tokenFactory: contractAddresses[1] as Address,
-    dopplerFactory: contractAddresses[2] as Address,
-    governanceFactory: contractAddresses[3] as Address,
-    migrator: contractAddresses[4] as Address,
-    poolManager: contractAddresses[5] as Address,
-    stateView: contractAddresses[6] as Address,
-    customRouter: contractAddresses[7] as Address,
+    governanceFactory: contractAddresses[2] as Address,
+    liquidityMigrator: contractAddresses[3] as Address,
+    poolManager: contractAddresses[4] as Address,
+    v3Initializer: contractAddresses[5] as Address,
   };
 
   await testClient.mine({
