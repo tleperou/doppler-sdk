20dba771b6efed0532313a97ceb97347eef3deee
kinrezc <matt.czernik@gmail.com>
2025-02-05 15:33:34 -0500

    release + rm deprecated basic router entity

--- packages/doppler-v4-sdk/src/index.ts
+++ packages/doppler-v4-sdk/src/index.ts
@@ -1,8 +1,7 @@
 export * from './entities/doppler';
 export * from './entities/factory';
-export * from './entities/router';
 export * from './entities/token';
-export * from './abis'
+export * from './abis';
 
 export * from './types';
 export * from './abis';
