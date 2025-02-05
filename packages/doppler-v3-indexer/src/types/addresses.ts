b6bc70ec20a2e918221e2a58b3ee0d486d2bc39b
kinrezc <matt.czernik@gmail.com>
2025-02-05 18:23:11 -0500

    update v3 factory

--- packages/doppler-v3-indexer/src/types/addresses.ts
+++ packages/doppler-v3-indexer/src/types/addresses.ts
@@ -35,22 +35,22 @@ export type OracleAddresses = {
 };
 
 export const v3Addresses: V3Addresses = {
-  v3Initializer: "0xdf6f19077cba70fb4f43fa609247ad7dda7a9a1c" as Address,
+  v3Initializer: "0x7Fb9a622186B4660A5988C223ebb9d3690dD5007" as Address,
 };
 
 export const v4Addresses: V4Addresses = {
-  poolManager: "0x9F18932969e072e642a0c73D98b97Abf99276e82" as Address,
-  dopplerDeployer: "0x03414fe5722fe0c699f9dcab97fb28133df2a86a" as Address,
-  v4Initializer: "0xca01bec6faeef04351a1f5b4045883e5f91a97b3" as Address,
-  stateView: "0x02dc4a94f68b0f76046cdeb0e202d8854e203ec3" as Address,
+  poolManager: "0x00B036B58a818B1BC34d502D3fE730Db729e62AC" as Address,
+  dopplerDeployer: "0x8350cAd81149A9944c2fb4276955FaAA7D61e836" as Address,
+  v4Initializer: "0x992375478626E67F4e639d3298EbCAaE51C3dF0b" as Address,
+  stateView: "0xc199F1072a74D4e905ABa1A84d9a45E2546B6222" as Address,
 };
 
 export const sharedAddresses: SharedAddresses = {
-  airlock: "0x2a6a1881b5cda2c444782a713d3979670df2206f" as Address,
-  tokenFactory: "0xf713fd0f91517a9f22b268b6f8d8deb88e8fea5b" as Address,
-  universalRouter: "0x34d5a9624c340f2cf4a2a0edc64f6fcadd65d475" as Address,
-  governanceFactory: "0x9442c17048b2c8bdd8ffdea1ac1d98e25106517b" as Address,
-  migrator: "0xa87301f5efc1e39b72c9e84114893a981e09277b" as Address,
+  airlock: "0x651ab94B4777e2e4cdf96082d90C65bd947b73A4" as Address,
+  tokenFactory: "0xC5E5a19a2ee32831Fcb8a81546979AF43936EbaA" as Address,
+  universalRouter: "0xf70536B3bcC1bD1a972dc186A2cf84cC6da6Be5D" as Address,
+  governanceFactory: "0x1E4332EEfAE9e4967C2D186f7b2d439D778e81cC" as Address,
+  migrator: "0x44C448E38A2C3D206c9132E7f645510dFbBC946b" as Address,
   weth: "0x4200000000000000000000000000000000000006" as Address,
 };
 
