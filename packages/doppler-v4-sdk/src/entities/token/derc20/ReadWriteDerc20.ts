e116fafc85bbf4a1a762f3d94731a975e6d104ff
Thomas Lepérou <thomas.leperou@gmail.com>
2025-04-02 14:28:32 +0700

    refactor: 💡 options release renounceOwnership methods

--- packages/doppler-v4-sdk/src/entities/token/derc20/ReadWriteDerc20.ts
+++ packages/doppler-v4-sdk/src/entities/token/derc20/ReadWriteDerc20.ts
@@ -3,6 +3,8 @@ import {
   ReadWriteAdapter,
   Drift,
   createDrift,
+  ContractWriteOptions,
+  OnMinedParam,
 } from '@delvtech/drift';
 import { Address, Hex } from 'viem';
 import { Derc20ABI, ReadDerc20 } from './ReadDerc20';
@@ -29,11 +31,16 @@ export class ReadWriteDerc20 extends ReadDerc20 {
     );
   }
 
-  async release(amount: bigint): Promise<Hex> {
-    return this.contract.write('release', { amount });
+  async release(
+    amount: bigint,
+    options?: ContractWriteOptions & OnMinedParam
+  ): Promise<Hex> {
+    return this.contract.write('release', { amount }, options);
   }
 
-  async renounceOwnership(): Promise<Hex> {
-    return this.contract.write('renounceOwnership');
+  async renounceOwnership(
+    options?: ContractWriteOptions & OnMinedParam
+  ): Promise<Hex> {
+    return this.contract.write('renounceOwnership', {}, options);
   }
 }
