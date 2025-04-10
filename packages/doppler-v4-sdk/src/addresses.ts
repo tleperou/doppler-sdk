7d9215b450a94bacba76bdb4e21d2027b614ae58
Matt <matt.czernik@gmail.com>
2025-04-09 20:00:42 -0400

    add base sepolia deployment addresses

--- packages/doppler-v4-sdk/src/addresses.ts
+++ packages/doppler-v4-sdk/src/addresses.ts
@@ -15,4 +15,16 @@ export const DOPPLER_V4_ADDRESSES: { [chainId: number]: DopplerV4Addresses } = {
     universalRouter: '0xf70536B3bcC1bD1a972dc186A2cf84cC6da6Be5D' as Address,
     stateView: '0xc199F1072a74D4e905ABa1A84d9a45E2546B6222' as Address,
   },
+  84532: {
+    poolManager: '0x05E73354cFDd6745C338b50BcFDfA3Aa6fA03408' as Address,
+    airlock: '0xE0dC4012AC9C868F09c6e4b20d66ED46D6F258d0' as Address,
+    tokenFactory: '0x4225C632b62622Bd7B0A3eC9745C0a866Ff94F6F' as Address,
+    dopplerDeployer: '0x6ddfED58D238Ca3195E49d8ac3d4cEa6386E5C33' as Address,
+    governanceFactory: '0x3AD727ee0FBBb8Ee0920933FdB96F23fD56f1299' as Address,
+    v4Initializer: '0x49F3fBB2dFF7f3d03B622e3b2a6d3F2E6fdB2a5A' as Address,
+    migrator: '0xA3C847eAb58eAa9cbc215C785c9cfBc19CDABD5f' as Address,
+    v3Initializer: '0x7Fb9a622186B4660A5988C223ebb9d3690dD5007' as Address,
+    universalRouter: '0x492e6456d9528771018deb9e87ef7750ef184104' as Address,
+    stateView: '0x571291b572ed32ce6751a2cb2486ebee8defb9b4' as Address,
+  },
 };
